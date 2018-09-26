42e6a04d44bdceac6ae8da39a3e564088ab90e90
lei.tian <lei.tian@uhouzz.com>
2018-09-26 10:54:48 +0800

    5.0.21.1

--- tests/script/install.sh
+++ tests/script/install.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
 
 if [ $(phpenv version-name) != "hhvm" ]; then
-    #cp tests/extensions/$(phpenv version-name)/*.so $(php-config --extension-dir)
-
-    if [ $(phpenv version-name) = "7.0" ]; then
-        phpenv config-add tests/conf/apcu.ini
-    else
-        phpenv config-add tests/conf/apcu.ini
-    fi
+    cp tests/extensions/$(phpenv version-name)/*.so $(php-config --extension-dir)
 
     phpenv config-add tests/conf/memcached.ini
     phpenv config-add tests/conf/redis.ini
